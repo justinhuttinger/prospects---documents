797e2c008743f8a31c71351ae096c4e5a0f52ac0
justinhuttinger <justin@wcstrength.com>
2025-11-13 08:38:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ console.log(`Prospect ID: ${prospectId}`);
       document: pdfBuffer.toString('base64'),
       documentName: `Waiver_${formData.first_name}_${formData.last_name}.pdf`,
       documentType: "pdf",
-      imageType: "contract",
+      imageType: "member_document",
       memberId: prospectId
     };
 
